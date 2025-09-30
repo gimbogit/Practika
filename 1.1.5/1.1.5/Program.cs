1f586629970e27e299156559a0352b4e0673327b
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-09-30 09:07:25 +0300

    Add files via upload

---
+++ 1.1.5/1.1.5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _1._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+
+            
+                int randomNum = random.Next(10);
+
+                Console.WriteLine("Попробуйте угадать число загаданное программой: ");
+                int peopleNum = Convert.ToInt32(Console.ReadLine());
+
+                if (peopleNum == randomNum)
+                {
+                    Console.WriteLine("ПОЗДРАВЛЯЮ!!!! ВЫ УГАДАЛИ !!!!");
+                }
+                else 
+                {
+                    Console.WriteLine("Вы не угадали((((");
+                }
+                
+            }
+        }
+    }
+}
