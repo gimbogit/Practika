6c8fc0761e578a35f23436788963e1f5947509f4
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-09-30 12:39:48 +0300

    Add files via upload

---
+++ 1.3.2/1.3.2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _1._3._2
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        
+        public static int[] MadeToArray(int maxSum)
+        {
+            int sum = 0;
+            int count = 0;
+            int[] tempArray = new int[100]; 
+            while (sum <= maxSum)
+            {
+                int value = random.Next(1, 10); 
+                sum += value;
+                if (sum > maxSum) break;
+                tempArray[count++] = value;
+            }
+            int[] resultArray = new int[count];
+            Array.Copy(tempArray, resultArray, count);
+            return resultArray;
+        }
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Введите максимальную сумму: ");
+                int maxSum = int.Parse(Console.ReadLine());
+
+                int[] array = MadeToArray(maxSum);
+                Console.WriteLine("Сгенерированный массив:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+    }
+}
