d032c4543d7dab129ba60ee91af654f89871f186
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-09-30 11:51:31 +0300

    Add files via upload

---
+++ 1.2.6/1.2.6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1._2._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double[] array = new double[10];
+                Random rnd = new Random();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.NextDouble() * 20 - 10;
+                    Console.WriteLine($"{i}: {array[i]:F2}");
+                }
+
+                var indices = array
+        .Select((value, index) => new { value, index })
+        .OrderBy(x => x.value)
+        .Select(x => x.index)
+        .ToArray();
+                Console.WriteLine("Массив индексов в порядке возрастания значений:");
+                foreach (var index in indices)
+                {
+                    Console.Write(index + " ");
+                }
+                Console.ReadKey();
+                Console.Clear();    
+            }
+        }
+
+    }
+}
---
+++ 1.3.1/1.3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите значение числителя: ");
+                int chisl = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите значение знаменателя: ");
+                int znam = Convert.ToInt32(Console.ReadLine());
+
+                int nod = SearchNOD(chisl, znam);
+                chisl = chisl / nod;
+                znam = znam / nod;
+                Console.WriteLine($"Полученная сокращенная дробь: {chisl}/{znam}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        public static int SearchNOD(int a, int b)
+        {
+            while(b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+            
+    }
+}
