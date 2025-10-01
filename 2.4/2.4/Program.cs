df0ee53852c09470e5235654cc15e2af5914ff9f
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-10-01 12:44:13 +0300

    Add files via upload

---
+++ 2.4/2.4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface IDrawable
+{
+    void Draw();
+}
+
+class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Создается круг ");
+    }
+}
+
+class Rectangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Создается прямоугольник");
+    }
+}
+
+class Triangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Создается Треугольник");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IDrawable[] shapes = new IDrawable[]
+        {
+            new Circle(),
+            new Rectangle(),
+            new Triangle()
+        };
+
+        foreach (IDrawable shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
