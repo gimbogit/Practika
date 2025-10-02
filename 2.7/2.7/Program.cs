d510f75d732d14a746d98f020623f226dc3457c3
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-10-02 09:07:10 +0300

    Add files via upload

---
+++ 2.7/2.7/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guitar guitar1 = new Guitar("Gibson","2540");
+            guitar1.Dispose();
+            Guitar guitar2 = new Guitar("Jet", "560");
+            guitar2.Dispose();
+        }
+
+         public class Guitar: IDisposable {
+        
+            private string _model;
+            private string _price;
+
+            public Guitar(string model, string price)
+            {
+                _model = model;
+                _price = price;
+                Console.WriteLine($"Добавлена гитара {model} по доступной цене в {price} рублей");
+            }
+            //~Guitar() 
+            //{
+            //    Console.WriteLine($"Гитара {_model} удалена ");
+            //}
+            public void Dispose()
+            {
+                Console.WriteLine($"Гитара {_model} удалена ");
+            }
+            }
+    }
+}
