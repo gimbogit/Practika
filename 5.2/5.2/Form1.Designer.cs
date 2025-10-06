81a25e5a2e055969bab4d6400e4410402aa5b84f
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-10-06 10:23:45 +0300

    Add files via upload

---
+++ 5.2/5.2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _5._2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem открытьToolStripMenuItem;
+        private ToolStripMenuItem сохранитьToolStripMenuItem;
+        private ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private RichTextBox richTextBox1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new MenuStrip();
+            this.файлToolStripMenuItem = new ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new ToolStripMenuItem();
+            this.выходToolStripMenuItem = new ToolStripMenuItem();
+            this.richTextBox1 = new RichTextBox();
+            this.openFileDialog1 = new OpenFileDialog();
+            this.saveFileDialog1 = new SaveFileDialog();
+
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+
+            // menuStrip1
+            this.menuStrip1.Items.AddRange(new ToolStripItem[] { this.файлToolStripMenuItem });
+            this.menuStrip1.Location = new Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+
+            // файлToolStripMenuItem
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] {
+                this.открытьToolStripMenuItem,
+                this.сохранитьToolStripMenuItem,
+                this.сохранитьКакToolStripMenuItem,
+                this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+
+            // открытьToolStripMenuItem
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            this.открытьToolStripMenuItem.Size = new Size(224, 24);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new EventHandler(this.открытьToolStripMenuItem_Click);
+
+            // сохранитьToolStripMenuItem
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            this.сохранитьToolStripMenuItem.Size = new Size(224, 24);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new EventHandler(this.сохранитьToolStripMenuItem_Click);
+
+            // сохранитьКакToolStripMenuItem
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.S;
+            this.сохранитьКакToolStripMenuItem.Size = new Size(224, 24);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как";
+            this.сохранитьКакToolStripMenuItem.Click += new EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+
+            // выходToolStripMenuItem
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new Size(224, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new EventHandler(this.выходToolStripMenuItem_Click);
+
+            // richTextBox1
+            this.richTextBox1.Dock = DockStyle.Fill;
+            this.richTextBox1.Location = new Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new Size(800, 422);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+
+            // openFileDialog1
+            this.openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            this.openFileDialog1.Title = "Открыть файл";
+
+            // saveFileDialog1
+            this.saveFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            this.saveFileDialog1.Title = "Сохранить файл";
+
+            // Form1
+            this.AutoScaleDimensions = new SizeF(8F, 16F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Текстовый редактор";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ 5.2/5.2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5._2
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    currentFilePath = openFileDialog1.FileName;
+                    richTextBox1.Text = File.ReadAllText(currentFilePath, Encoding.GetEncoding(1251));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при открытии файла: " + ex.Message);
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                сохранитьКакToolStripMenuItem_Click(sender, e);
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText(currentFilePath, richTextBox1.Text, Encoding.GetEncoding(1251));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    currentFilePath = saveFileDialog1.FileName;
+                    File.WriteAllText(currentFilePath, richTextBox1.Text, Encoding.GetEncoding(1251));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
+                }
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
