730c1903b24b5c8ad3ab9ae45de340c1981efeb6
gimbogit <142109908+gimbogit@users.noreply.github.com>
2025-10-06 09:58:05 +0300

    Add files via upload

---
+++ 5.1/5.1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panelTools = new System.Windows.Forms.Panel();
+            this.panelColor = new System.Windows.Forms.Panel();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.panelTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(10, 10);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(80, 30);
+            this.btnLine.TabIndex = 0;
+            this.btnLine.Text = "Линия";
+            this.btnLine.UseVisualStyleBackColor = true;
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(100, 10);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(80, 30);
+            this.btnCircle.TabIndex = 1;
+            this.btnCircle.Text = "Круг";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(190, 10);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(80, 30);
+            this.btnRectangle.TabIndex = 2;
+            this.btnRectangle.Text = "Квадрат";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(370, 10);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 30);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // panelTools
+            // 
+            this.panelTools.Controls.Add(this.btnColor);
+            this.panelTools.Controls.Add(this.panelColor);
+            this.panelTools.Controls.Add(this.btnLine);
+            this.panelTools.Controls.Add(this.btnClear);
+            this.panelTools.Controls.Add(this.btnCircle);
+            this.panelTools.Controls.Add(this.btnRectangle);
+            this.panelTools.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTools.Location = new System.Drawing.Point(0, 0);
+            this.panelTools.Name = "panelTools";
+            this.panelTools.Size = new System.Drawing.Size(800, 50);
+            this.panelTools.TabIndex = 4;
+            // 
+            // panelColor
+            // 
+            this.panelColor.BackColor = System.Drawing.Color.Black;
+            this.panelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelColor.Location = new System.Drawing.Point(290, 15);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(20, 20);
+            this.panelColor.TabIndex = 4;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(280, 10);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(80, 30);
+            this.btnColor.TabIndex = 5;
+            this.btnColor.Text = "Цвет";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelTools);
+            this.Name = "Form1";
+            this.Text = "Рисовалка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelTools.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Panel panelTools;
+        private System.Windows.Forms.Panel panelColor;
+        private System.Windows.Forms.Button btnColor;
+    }
+}
---
+++ 5.1/5.1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _5._1
+{
+    public partial class Form1 : Form
+    {
+        public enum DrawMode { Line, Circle, Rectangle }
+        private DrawMode currentMode = DrawMode.Line;
+        private Point startPoint;
+        private Point endPoint;
+        private bool isDrawing = false;
+        private List<Shape> shapes = new List<Shape>();
+        private Pen drawingPen = new Pen(Color.Black, 2);
+        private ColorDialog colorDialog = new ColorDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupEventHandlers();
+            this.DoubleBuffered = true;
+        }
+
+        private void SetupEventHandlers()
+        {
+            btnLine.Click += (s, e) => currentMode = DrawMode.Line;
+            btnCircle.Click += (s, e) => currentMode = DrawMode.Circle;
+            btnRectangle.Click += (s, e) => currentMode = DrawMode.Rectangle;
+            btnClear.Click += (s, e) => { shapes.Clear(); this.Invalidate(); };
+            btnColor.Click += BtnColor_Click;
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingPen.Color = colorDialog.Color;
+                panelColor.BackColor = colorDialog.Color;
+            }
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPoint = e.Location;
+                isDrawing = true;
+            }
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                endPoint = e.Location;
+                this.Invalidate();
+            }
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            if (isDrawing && e.Button == MouseButtons.Left)
+            {
+                endPoint = e.Location;
+
+                Shape newShape = new Shape(currentMode, startPoint, endPoint, drawingPen.Color);
+                shapes.Add(newShape);
+
+                isDrawing = false;
+                this.Invalidate();
+            }
+            base.OnMouseUp(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Рисуем все сохраненные фигуры
+            for (int i = 0; i < shapes.Count; i++)
+            {
+                DrawShape(e.Graphics, shapes[i]);
+            }
+
+            // Рисуем текущую фигуру (предпросмотр)
+            if (isDrawing)
+            {
+                Shape tempShape = new Shape(currentMode, startPoint, endPoint, drawingPen.Color);
+                DrawShape(e.Graphics, tempShape);
+            }
+
+            base.OnPaint(e);
+        }
+
+        private void DrawShape(Graphics g, Shape shape)
+        {
+            using (Pen shapePen = new Pen(shape.ShapeColor, 2))
+            {
+                Point start = shape.StartPoint;
+                Point end = shape.EndPoint;
+
+                switch (shape.Type)
+                {
+                    case DrawMode.Line:
+                        g.DrawLine(shapePen, start, end);
+                        break;
+                    case DrawMode.Circle:
+                        int circleWidth = Math.Abs(end.X - start.X);
+                        int circleHeight = Math.Abs(end.Y - start.Y);
+                        int circleX = start.X < end.X ? start.X : end.X;
+                        int circleY = start.Y < end.Y ? start.Y : end.Y;
+                        g.DrawEllipse(shapePen, circleX, circleY, circleWidth, circleHeight);
+                        break;
+                    case DrawMode.Rectangle:
+                        int rectSize = GetMinSize(Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y));
+                        int rectX = start.X < end.X ? start.X : start.X - rectSize;
+                        int rectY = start.Y < end.Y ? start.Y : start.Y - rectSize;
+                        g.DrawRectangle(shapePen, rectX, rectY, rectSize, rectSize);
+                        break;
+                }
+            }
+        }
+
+        private int GetMinSize(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Дополнительная инициализация при загрузке формы
+        }
+    }
+
+    public class Shape
+    {
+        public Form1.DrawMode Type { get; set; }
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+        public Color ShapeColor { get; set; }
+
+        public Shape(Form1.DrawMode type, Point start, Point end, Color color)
+        {
+            Type = type;
+            StartPoint = start;
+            EndPoint = end;
+            ShapeColor = color;
+        }
+    }
+}
